f7327e3a7ab218d041070c22286a570fe38c3af0
KEVIN COLE <kcole93@gmail.com>
2024-01-24 21:15:01 -0500

    Add basic auth

--- app.js
+++ app.js
@@ -2,14 +2,45 @@ const express = require('express');
 const axios = require('axios'); // For making HTTP requests
 const { Readability } = require('readability');
 const { JSDOM } = require('jsdom');
+require('dotenv').config();
+
+// Set API_KEY from Environment Variable
+const apiKey = process.env.API_KEY;
+
 
 const app = express();
 const port = 3000; // This should match the EXPOSED port in your Dockerfile
 
 app.use(express.json());
+// Middleware to check and parse the Authorization header
+function checkAuthorizationHeader(req, res, next) {
+  const authHeader = req.headers.authorization;
 
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Authorization header missing' });
+  }
+
+  const [prefix, token] = authHeader.split(' ');
+
+  if (prefix.toLowerCase() !== 'bearer') {
+    return res.status(401).json({ error: 'Invalid authorization prefix' });
+  }
+
+  // Attach the token to the request for use in route handlers
+  req.token = token;
+
+  // Continue to the next middleware or route handler
+  next();
+}
+
+app.use('/parse-url', checkAuthorizationHeader);
 app.post('/parse-url', async (req, res) => {
-  const url = req.body.url; // Assuming you send the URL as a JSON object
+  // Check if the API key is valid
+  if (req.token != apiKey) {
+    return res.status(401).json({ error: 'Unauthorized. Invalid API key.' });
+  }
+
+  const url = req.body.url; // Send the URL as a JSON object
 
   try {
     // Fetch the HTML content from the provided URL
@@ -20,6 +51,7 @@ app.post('/parse-url', async (req, res) => {
     const reader = new Readability(doc.window.document);
     const article = reader.parse();
 
+    // Return the parsed article as a response
     res.json(article);
   } catch (error) {
     res.status(500).json({ error: 'Unable to fetch and parse the URL.' });
